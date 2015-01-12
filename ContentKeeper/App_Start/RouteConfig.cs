0db0cf11f03bdce30f190175692af6ffde1d3e52
Daniel Lindås <daniel.lindas@miconagruppen.se>
2015-01-12 11:32:17 +0100

    Cleaning out the closet

--- ContentKeeper/App_Start/RouteConfig.cs
+++ ContentKeeper/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ContentKeeper
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "GetContent", action = "ListAll", id = UrlParameter.Optional }
             );
         }
     }
