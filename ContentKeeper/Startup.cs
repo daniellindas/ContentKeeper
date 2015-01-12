46fcc59a33d962c499b6b65133d285bdf534d9d9
Daniel Lindås <daniel.lindas@miconagruppen.se>
2015-01-12 11:22:32 +0100

    First commit

---
+++ ContentKeeper/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContentKeeper.Startup))]
+namespace ContentKeeper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
